dc599f633318de47c7f55c8c5bbfa304743f196d
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-09-19 10:52:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const inputElement = document.querySelector("input")
+
+function addingEventListener(element) {
+  element.addEventListener("click", function(event){
+    alert("I am clicked!")
+  })
 }
+
+addingEventListener(inputElement)
